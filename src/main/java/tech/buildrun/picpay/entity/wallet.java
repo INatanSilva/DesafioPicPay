db42fc20acb7699aa73b9bbcd378bb61edb7823a
Natan Silva <92834066natan@gmail.com>
2024-08-12 02:04:15 +0100

    Entidades Mapeadas

--- src/main/java/tech/buildrun/picpay/entity/wallet.java
+++ src/main/java/tech/buildrun/picpay/entity/wallet.java
@@ -2,11 +2,7 @@ package tech.buildrun.picpay.entity;
 
 import java.math.BigDecimal;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_wallet")
@@ -16,14 +12,81 @@ public class wallet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "full_name")
     private String fullName;
 
+    @Column(name = "cpf_cnpj", unique = true)
     private String cpfCnpj;
 
+    @Column(unique = true)
     private String email;
 
+    @Column(name = "password")
     private String password;
 
-    private BigDecimal balance;
+    @Column(name = "balance")
+    private BigDecimal balance = BigDecimal.ZERO;
 
+    @ManyToOne
+    @JoinColumn(name = "wallet_type_id")
+    private walletType walletType;
+
+    public wallet() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public tech.buildrun.picpay.entity.walletType getWalletType() {
+        return walletType;
+    }
+
+    public void setWalletType(tech.buildrun.picpay.entity.walletType walletType) {
+        this.walletType = walletType;
+    }
 }
--- src/main/java/tech/buildrun/picpay/entity/walletType.java
+++ src/main/java/tech/buildrun/picpay/entity/walletType.java
@@ -1,9 +1,6 @@
 package tech.buildrun.picpay.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_wallet_type")
@@ -15,5 +12,22 @@ public class walletType {
 
     private String description;
 
-    
+    public walletType() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
